0e2ffc9247050d87b6e4398106f9899af81f4af3
zauner <sagishi@zive.at>
2012-05-15 11:44:05 +0200

    adds 'Help' headline to help shelf

--- show.php
+++ show.php
@@ -65,6 +65,7 @@ $name = $db->get("name");
 </div>
 
 <div class="shelf" id="help_shelf">
+  <h2>Help</h2>
   <p>
     Here at the <b>VVVV.js lab</b>, you can <b>push every patch to a VVVV instance</b> on your machine, play around with it, and <b>save it as a new revision</b>. Don't worry, you won't break or overwrite anything doing so.<br/>
   </p>
